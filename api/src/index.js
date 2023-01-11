d965dadbe2bf5645d462bc3f0b59ae213f4ea29a
rishianand39 <rishi.rn818@gmail.com>
2023-01-12 00:54:08 +0530

    server file removed

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,10 @@
 const express =require("express")
 const cors=require("cors")
 const app = express();
+const connect =require("./configs/db");
+const dotenv=require("dotenv")
+dotenv.config();
+
 
 app.use(cors())
 app.use(express.json());
@@ -14,5 +18,12 @@ const userController=require("./controllers/user.controller")
 app.use("/", userController)
 
 
-
-module.exports=app
+const PORT =process.env.PORT || 8000;
+app.listen(PORT,async()=>{
+    try {
+        await connect;
+        console.log(`server running on port ${PORT}`);
+    } catch (error) {
+        console.log(error);
+    }
+})
